d7220c6a01042de1ebadb767d2a0416d18d97a05
asumin <bardex@ya.ru>
2017-04-20 00:49:03 +0300

    phpunit bootstrap drop/create test index

--- tests/Bardex/Tests/bootstrap.php
+++ tests/Bardex/Tests/bootstrap.php
@@ -13,13 +13,17 @@ $client = $builder->build();
 
 \Bardex\Tests\AbstractTestCase::setClient($client, $index, $type);
 
+if ( $client->indices()->exists(['index' => $index]) ) {
+    $client->indices()->delete(['index' => $index]);
+}
+
 // create test index
 $params = [
     'index' => $index,
     'body'  => [
         'settings' => [
-            'number_of_shards'   => 1,
-            'number_of_replicas' => 1
+            'number_of_shards'   => 5,
+            'number_of_replicas' => 0
         ]
     ]
 ];
