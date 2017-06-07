972a62fa1b848b20bf75543590230f5685e173b6
asumin <bardex@ya.ru>
2017-06-07 09:37:12 +0300

    docs

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Bardex\\Elastic\\Query"],["c","JsonSerializable"]];
+ApiGen.elements = [["c","ArrayAccess"],["c","Bardex\\Elastic\\IListener"],["c","Bardex\\Elastic\\Listener\\Logger"],["c","Bardex\\Elastic\\MultiQuery"],["c","Bardex\\Elastic\\Query"],["c","Bardex\\Elastic\\Script"],["c","Bardex\\Elastic\\SearchQuery"],["c","Bardex\\Elastic\\SearchResult"],["c","Bardex\\Elastic\\Where"],["c","Countable"],["c","Exception"],["c","IteratorAggregate"],["c","Throwable"],["c","Traversable"]];
