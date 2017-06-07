b12ffb553a556b980960ffee885ae665353bf888
asumin <bardex@ya.ru>
2017-06-07 08:30:24 +0300

    unittest for Where

--- tests/Bardex/Tests/WhereTest.php
+++ tests/Bardex/Tests/WhereTest.php
@@ -10,154 +10,41 @@ class WhereTest extends AbstractTestCase
 {
     protected static $testdata = [
         [
+            'id' => 2,
+            'title' => 'Happy friends',
+            'channels' => [75],
+            'publicDate' => '2002-01-01T00:00:00+03:00',
+        ],
+        [
             'id' => 20,
             'title' => 'title Alice record',
             'channels' => [1,2,3],
             'publicDate' => '2017-01-01T00:00:00+03:00',
-            'tags' => [
-                [
-                    'id' => 15,
-                    'title' => 'pop'
-                ]
-            ]
         ],
         [
             'id' => 10,
             'title' => 'title Bob record',
+            'anons' => 'Bob is Alice friend',
             'channels' => [1],
             'publicDate' => '2016-12-31T23:00:00+03:00',
         ]
     ];
 
     protected $mytests = [
-        [
-            'name'       => 'whereEqual',
-            'method'     => 'equal',
-            'field'      => 'id',
-            'params'     => [20],
-            'validCount' => 1,
-            'validId'    => [20]
-        ],
-        [
-            'name'       => 'whereInOne',
-            'method'     => 'in',
-            'field'      => 'channels',
-            'params'     => [[2]],
-            'validCount' => 1,
-            'validId'    => [20]
-        ],
-        [
-            'name'       => 'whereInNothing',
-            'method'     => 'in',
-            'field'      => 'channels',
-            'params'     => [[5,6]],
-            'validCount' => 0,
-            'validId'    => []
-        ],
-        [
-            'name'       => 'whereInBoth',
-            'method'     => 'in',
-            'field'      => 'channels',
-            'params'     => [[1,2]],
-            'validCount' => 2,
-            'validId'    => [20,10]
-        ],
-        [
-            'name'       => 'whereMatchOne',
-            'method'     => 'match',
-            'field'      => 'title',
-            'params'     => ['alice'],
-            'validCount' => 1,
-            'validId'    => [20]
-        ],
-        [
-            'name'       => 'whereMatchBoth',
-            'method'     => 'match',
-            'field'      => 'title',
-            'params'     => ['record'],
-            'validCount' => 2,
-            'validId'    => [20, 10]
-        ],
-        [
-            'name'       => 'whereMatchNothing',
-            'method'     => 'match',
-            'field'      => 'title',
-            'params'     => ['zitta'],
-            'validCount' => 0,
-            'validId'    => []
-        ],
-        [
-            'name'       => 'whereBetweenOne',
-            'method'     => 'between',
-            'field'      => 'id',
-            'params'     => [15, 25],
-            'validCount' => 1,
-            'validId'    => [20]
-        ],
-        [
-            'name'       => 'whereBetweenBoth',
-            'method'     => 'between',
-            'field'      => 'id',
-            'params'     => [10, 20],
-            'validCount' => 2,
-            'validId'    => [20, 10]
-        ],
-        [
-            'name'       => 'whereBetweenNothing',
-            'method'     => 'between',
-            'field'      => 'id',
-            'params'     => [50, 60],
-            'validCount' => 0,
-            'validId'    => []
-        ],
-        [
-            'name'       => 'whereBetweenDateOne',
-            'method'     => 'between',
-            'field'      => 'publicDate',
-            'params'     => ['2017-01-01T00:00:00+03:00', '2017-01-01T00:00:00+03:00', 'date_time_no_millis'],
-            'validCount' => 1,
-            'validId'    => [20]
-        ],
-        [
-            'name'       => 'whereBetweenDateTwo',
-            'method'     => 'between',
-            'field'      => 'publicDate',
-            'params'     => ['2016-12-31T00:00:00+03:00', '2017-01-01T00:00:00+03:00', 'date_time_no_millis'],
-            'validCount' => 2,
-            'validId'    => [20,10]
-        ],
-        [
-            'name'       => 'whereBetweenDateNothing',
-            'method'     => 'between',
-            'field'      => 'publicDate',
-            'params'     => ['2016-12-31T00:00:00+03:00', '2016-12-31T12:00:00+03:00', 'date_time_no_millis'],
-            'validCount' => 0,
-            'validId'    => []
-        ],
-        [
-            'name'       => 'whereGreaterOne',
-            'method'     => 'greater',
-            'field'      => 'id',
-            'params'     => [15],
-            'validCount' => 1,
-            'validId'    => [20]
-        ],
-        [
-            'name'       => 'whereGreaterBoth',
-            'method'     => 'greater',
-            'field'      => 'id',
-            'params'     => [0],
-            'validCount' => 2,
-            'validId'    => [20, 10]
-        ],
-        [
-            'name'       => 'whereGreaterNothing',
-            'method'     => 'greater',
-            'field'      => 'id',
-            'params'     => [20],
-            'validCount' => 0,
-            'validId'    => []
-        ],
+        ['name' => 'equal', 'method' => 'equal', 'field' => 'id',       'params' => [20],      'validCount' => 1, 'validId' => [20]],
+        ['name' => 'in',    'method' => 'in',    'field' => 'channels', 'params' => [[2]],     'validCount' => 1, 'validId' => [20]],
+        ['name' => 'match', 'method' => 'match', 'field' => 'title',    'params' => ['alice'], 'validCount' => 1, 'validId' => [20]],
+        ['name' => 'match multi fields', 'method' => 'match', 'field' => ['title','anons'], 'params' => ['alice'], 'validCount' => 2, 'validId' => [20, 10]],
+        ['name' => 'between',      'method' => 'between', 'field' => 'id', 'params' => [15, 25], 'validCount' => 1, 'validId' => [20]],
+        ['name' => 'greater',      'method' => 'greater', 'field' => 'id', 'params' => [15], 'validCount' => 1, 'validId' => [20]],
+        ['name' => 'greaterEqual', 'method' => 'greaterOrEqual', 'field' => 'id', 'params' => [10], 'validCount' => 2, 'validId' => [10,20]],
+        ['name' => 'less',         'method' => 'less', 'field' => 'id', 'params' => [10], 'validCount' => 1, 'validId' => [2]],
+        ['name' => 'lessEqual',    'method' => 'lessOrEqual', 'field' => 'id', 'params' => [10], 'validCount' => 2, 'validId' => [10,2]],
+        ['name' => 'between date', 'method' => 'between',     'field' => 'publicDate', 'params' => ['2016-12-31T23:00:00+03:00','2016-12-31T23:00:00+03:00'], 'validCount' => 1, 'validId' => [10]],
+        ['name' => 'greater date', 'method' => 'greater',     'field' => 'publicDate', 'params' => ['2016-12-31T23:00:00+03:00'], 'validCount' => 1, 'validId' => [20]],
+        ['name' => 'greaterEqual date', 'method' => 'greaterOrEqual', 'field' => 'publicDate', 'params' => ['2016-12-31T23:00:00+03:00'], 'validCount' => 2, 'validId' => [10,20]],
+        ['name' => 'less date',         'method' => 'less',           'field' => 'publicDate', 'params' => ['2016-12-31T23:00:00+03:00'], 'validCount' => 1, 'validId' => [2]],
+        ['name' => 'lessEqual date',    'method' => 'lessOrEqual',    'field' => 'publicDate', 'params' => ['2016-12-31T23:00:00+03:00'], 'validCount' => 2, 'validId' => [10,2]],
     ];
 
     public function testInstanceOf()
@@ -165,6 +52,7 @@ class WhereTest extends AbstractTestCase
         $query = $this->createQuery();
         $this->assertInstanceOf(SearchQuery::class, $query);
     }
+
 
     public function testWhereMethods()
     {
@@ -176,13 +64,13 @@ class WhereTest extends AbstractTestCase
 
             $this->assertInstanceOf(SearchResult::class, $results);
 
-            $this->assertCount($test['validCount'], $results, "test: ".$test['name']);
-            $this->assertEquals($test['validCount'], $results->getTotalCount(), "test: ".$test['name']);
+            $this->assertCount($test['validCount'], $results, "test method: ".$test['name']);
+            $this->assertEquals($test['validCount'], $results->getTotalCount(), "test method: ".$test['name']);
             foreach ($results as $result) {
-                $this->assertContains($result['id'], $test['validId'], "test: ".$test['name']);
+                $this->assertContains($result['id'], $test['validId'], "test method: ".$test['name']);
             }
 
-            $this->assertEquals(count($test['validId']), $test['validCount'], "test: ".$test['name']);
+            $this->assertEquals(count($test['validId']), $test['validCount'], "test method: ".$test['name']);
         }
     }
 
