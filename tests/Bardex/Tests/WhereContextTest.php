8ad304b27c2a2c0ed548c73edf4ef2b4705fded4
bardex <bardex@ya.ru>
2018-08-29 00:53:55 +0300

    update tests for ES-6

--- tests/Bardex/Tests/WhereContextTest.php
+++ tests/Bardex/Tests/WhereContextTest.php
@@ -7,7 +7,7 @@ class WhereContextTest extends AbstractTestCase
     protected static $testdata = [
         [
             'id' => 1,
-            'title' => 'Happy friends',
+            'title' => 'title Happy friends',
             'channels' => [1],
             'status' => 0,
         ],
@@ -30,40 +30,32 @@ class WhereContextTest extends AbstractTestCase
     public function testFilterContext()
     {
         $query = $this->createQuery();
-        $query->where('title', Query::CONTEXT_FILTER)->match('title')
-              ->where('channels', Query::CONTEXT_FILTER)->in([1,2,3])
-              ->setOrderBy(Query::ORDER_BY_SCORE, 'desc') //for filter context _score = 0, order not work
-              ->addOrderBy('id', 'asc');
+        $query->where('status', Query::CONTEXT_FILTER)->equal(1)
+              ->where('channels', Query::CONTEXT_FILTER)->in([1,2,3]);
 
         $results = $query->fetchAll();
 
-        $this->assertEquals(2, $results->getTotalFound());
-
+        $this->assertEquals(1, $results->getTotalFound());
         $this->assertEquals(2, $results[0]['id']);
-        $this->assertEquals(3, $results[1]['id']);
     }
 
     public function testMustContext()
     {
         $query = $this->createQuery();
         $query->where('title', Query::CONTEXT_MUST)->match('title')
-            ->where('channels', Query::CONTEXT_MUST)->in([1,2,3])
-            ->setOrderBy(Query::ORDER_BY_SCORE, 'desc') //for must context order work
-            ->addOrderBy('id', 'asc');
+            ->where('channels', Query::CONTEXT_MUST)->in([2]);
 
         $results = $query->fetchAll();
 
-        $this->assertEquals(2, $results->getTotalFound());
-
+        $this->assertEquals(1, $results->getTotalFound());
         $this->assertEquals(3, $results[0]['id']);
-        $this->assertEquals(2, $results[1]['id']);
     }
 
     public function testShouldContext()
     {
         $query = $this->createQuery();
-        $query->where('title', Query::CONTEXT_SHOULD)->match('title')
-            ->where('channels', Query::CONTEXT_SHOULD)->in([1,2,3])
+        $query->where('title', Query::CONTEXT_SHOULD)->match('title alice')
+            ->where('channels', Query::CONTEXT_SHOULD)->in([2,3])
             ->setOrderBy(Query::ORDER_BY_SCORE, 'desc') //for should context order work
             ->addOrderBy('id', 'asc');
 
@@ -82,14 +74,12 @@ class WhereContextTest extends AbstractTestCase
         $query->where('status', Query::CONTEXT_FILTER)->equal(0) // filtered by status
             ->where('title', Query::CONTEXT_SHOULD)->match('title') // title OR channels must match
             ->where('channels', Query::CONTEXT_SHOULD)->equal(1)
-            ->setOrderBy(Query::ORDER_BY_SCORE, 'desc') // order by score
             ->addOrderBy('id', 'asc');
 
         $results = $query->fetchAll();
 
         $this->assertEquals(2, $results->getTotalFound());
-
-        $this->assertEquals(3, $results[0]['id']);
-        $this->assertEquals(1, $results[1]['id']);
+        $this->assertEquals(1, $results[0]['id']);
+        $this->assertEquals(3, $results[1]['id']);
     }
 }
